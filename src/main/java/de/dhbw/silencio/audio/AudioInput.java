31d2c47d7cdc315c45497825f05aa069de7c76b4
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-11-15 17:05:33 +0100

    feat: remove threads

--- src/main/java/de/dhbw/silencio/audio/AudioInput.java
+++ src/main/java/de/dhbw/silencio/audio/AudioInput.java
@@ -2,15 +2,12 @@ package de.dhbw.silencio.audio;
 
 import javax.sound.sampled.AudioFormat;
 import java.util.Arrays;
-import java.util.concurrent.*;
 
 public class AudioInput {
     private static final String MICROPHONE_1 = "Microphone 1";
     private static final String MICROPHONE_2 = "Microphone 2";
 
     private static final int BUFFER_SIZE = 4096;
-
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public static void main(String[] args) {
         new AudioInput().run();
@@ -35,17 +32,10 @@ public class AudioInput {
             m2.listen();
 
             while (true) {
-                Future<byte[]> futureM1 = executor.submit(() -> m1.getNextChunk(BUFFER_SIZE));
-                Future<byte[]> futureM2 = executor.submit(() -> m2.getNextChunk(BUFFER_SIZE));
-
-                if (futureM1.isDone() && futureM2.isDone()) {
-                    System.out.printf("Microphone 1: %s%n", Arrays.toString(futureM1.get()));
-                    System.out.printf("Microphone 2: %s%n", Arrays.toString(futureM2.get()));
-                    System.out.println();
-                }
+                System.out.printf("Microphone 1: %s%n", Arrays.toString(m1.getNextChunk(BUFFER_SIZE)));
+                System.out.printf("Microphone 2: %s%n", Arrays.toString(m2.getNextChunk(BUFFER_SIZE)));
+                System.out.println();
             }
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 }
