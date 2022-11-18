58a6940365e9b5de74e8431df3e225bc30bf4c15
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-11-18 17:54:20 +0100

    feat: add FFT

--- src/main/java/de/dhbw/silencio/audio/AudioInput.java
+++ src/main/java/de/dhbw/silencio/audio/AudioInput.java
@@ -1,13 +1,12 @@
 package de.dhbw.silencio.audio;
 
 import javax.sound.sampled.AudioFormat;
-import java.util.Arrays;
 
 public class AudioInput {
     private static final String MICROPHONE_1 = "Microphone 1";
     private static final String MICROPHONE_2 = "Microphone 2";
 
-    private static final int BUFFER_SIZE = 48000;
+    private static final int BUFFER_SIZE = 65_536;
 
     public static void main(String[] args) {
         new AudioInput().run();
@@ -26,19 +25,50 @@ public class AudioInput {
 
         Microphone.printAvailableDevices();
 
-        try (Microphone m1 = new Microphone(MICROPHONE_1, format);
-             Microphone m2 = new Microphone(MICROPHONE_2, format)) {
-            m1.prepare();
-            m2.prepare();
+        try (Microphone microphone1 = new Microphone(MICROPHONE_1, format);
+             Microphone microphone2 = new Microphone(MICROPHONE_2, format)) {
+            microphone1.prepare();
+            microphone2.prepare();
 
-            m1.run();
-            m2.run();
+            microphone1.run();
+            microphone2.run();
 
             while (true) {
-                System.out.printf("Microphone 1: %s%n", Arrays.toString(m1.getNextChunk(BUFFER_SIZE)));
-                System.out.printf("Microphone 2: %s%n", Arrays.toString(m2.getNextChunk(BUFFER_SIZE)));
+                byte[] data1 = microphone1.getNextChunk(BUFFER_SIZE);
+                byte[] data2 = microphone2.getNextChunk(BUFFER_SIZE);
+
+                Complex[] cinput = new Complex[data1.length];
+                for (int i = 0; i < data1.length; i++) {
+                    cinput[i] = new Complex(data1[i], 0.0);
+                }
+
+                FastFourierTransformation.fft(cinput);
+
+                System.out.println("Results:");
+                for (Complex c : cinput) {
+                    System.out.println(c);
+                }
+
+//                System.out.printf("Microphone 1: %s%n", findMaxValue(data1));
+//                System.out.printf("Microphone 2: %s%n", findMaxValue(data2));
                 System.out.println();
             }
         }
+    }
+
+    private int findMaxValue(byte[] array) {
+        byte peak = 0;
+        int index = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            byte sample = (byte) Math.abs(array[i]);
+
+            if (sample > peak) {
+                peak = sample;
+                index = i;
+            }
+        }
+
+        return index;
     }
 }
