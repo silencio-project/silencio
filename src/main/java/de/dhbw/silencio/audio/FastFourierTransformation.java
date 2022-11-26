f566ba711b7c52b4330baa70e017c32af8bbe4b5
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-11-26 21:26:41 +0100

    feat: use Cooley–Tukey algorithm

--- src/main/java/de/dhbw/silencio/audio/FastFourierTransformation.java
+++ src/main/java/de/dhbw/silencio/audio/FastFourierTransformation.java
@@ -1,48 +1,49 @@
 package de.dhbw.silencio.audio;
 
-import static java.lang.Math.*;
-
+/**
+ * Implementation of the Fast Fourier Transformation (FFT).
+ *
+ * @author Yannick Kirschen
+ * @since 1.0.0
+ */
 public class FastFourierTransformation {
-    public static void fft(Complex[] buffer) {
-        int bits = (int) (log(buffer.length) / log(2));
-        for (int j = 1; j < buffer.length / 2; j++) {
-
-            int swapPos = bitReverse(j, bits);
-            Complex temp = buffer[j];
-            buffer[j] = buffer[swapPos];
-            buffer[swapPos] = temp;
+    /**
+     * Performs a Fast Fourier Transformation on a given array of complex numbers.
+     * <p>
+     * The Cooley–Tukey algorithm, named after J.W. Cooley and John Tukey, is the most common fast Fourier transform
+     * (FFT) algorithm. It re-expresses the discrete Fourier transform (DFT) of an arbitrary composite size N = N1N2 in
+     * terms of N1 smaller DFTs of sizes N2, recursively, to reduce the computation time to O(N log N) for highly
+     * composite N (smooth numbers).
+     * <p>
+     *
+     * @param coefficients numbers to perform FFT on (size must be power of 2)
+     * @see <a href="https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm">Cooley–Tukey Algorithm
+     * (Wikipedia)</a>
+     */
+    public static void transform(Complex[] coefficients) {
+        int size = coefficients.length;
+        if (size <= 1) {
+            return;
         }
 
-        for (int N = 2; N <= buffer.length; N <<= 1) {
-            for (int i = 0; i < buffer.length; i += N) {
-                for (int k = 0; k < N / 2; k++) {
+        Complex[] even = new Complex[size / 2];
+        Complex[] odd = new Complex[size / 2];
 
-                    int evenIndex = i + k;
-                    int oddIndex = i + k + (N / 2);
-                    Complex even = buffer[evenIndex];
-                    Complex odd = buffer[oddIndex];
-
-                    double term = (-2 * PI * k) / (double) N;
-                    Complex exp = (new Complex(cos(term), sin(term)).mult(odd));
-
-                    buffer[evenIndex] = even.add(exp);
-                    buffer[oddIndex] = even.sub(exp);
-                }
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                even[i / 2] = coefficients[i];
+            } else {
+                odd[(i - 1) / 2] = coefficients[i];
             }
         }
-    }
 
-    private static int bitReverse(int n, int bits) {
-        int reversedN = n;
-        int count = bits - 1;
+        transform(even);
+        transform(odd);
 
-        n >>= 1;
-        while (n > 0) {
-            reversedN = (reversedN << 1) | (n & 1);
-            count--;
-            n >>= 1;
+        for (int k = 0; k < size / 2; k++) {
+            Complex t = Complex.polar(1.0, -2 * Math.PI * k / size).multiply(odd[k]);
+            coefficients[k] = even[k].add(t);
+            coefficients[k + size / 2] = even[k].subtract(t);
         }
-
-        return ((reversedN << count) & ((1 << bits) - 1));
     }
 }
