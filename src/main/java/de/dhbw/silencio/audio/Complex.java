a0175e9d1996e5e196e9bb250252fa4dcb02695d
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-11-26 21:28:44 +0100

    feat(docs): add Javadoc

--- src/main/java/de/dhbw/silencio/audio/Complex.java
+++ src/main/java/de/dhbw/silencio/audio/Complex.java
@@ -1,33 +1,112 @@
 package de.dhbw.silencio.audio;
 
-class Complex {
-    public final double re;
-    public final double im;
+import lombok.EqualsAndHashCode;
 
+/**
+ * Value object for a complex number.
+ *
+ * @author Yannick Kirschen
+ * @since 1.0.0
+ */
+@EqualsAndHashCode
+public class Complex {
+    public final double real;
+    public final double imaginary;
+
+    /**
+     * Creates a complex number with both real and imaginary part 0.
+     */
     public Complex() {
         this(0, 0);
     }
 
+    /**
+     * Creates a complex number.
+     *
+     * @param r real part
+     * @param i imaginary part
+     */
     public Complex(double r, double i) {
-        re = r;
-        im = i;
+        real = r;
+        imaginary = i;
     }
 
+    /**
+     * Creates a complex number from an integer.
+     *
+     * @param i real number to transform into a complex number
+     * @return a complex number from an integer
+     */
+    public static Complex of(int i) {
+        return new Complex(i, 0.0);
+    }
+
+    /**
+     * Creates an array of complex numbers based on an array of integers.
+     *
+     * @param array array of integers to transform into an array of complex numbers
+     * @return an array of complex numbers from an array of integers
+     */
+    public static Complex[] of(int[] array) {
+        Complex[] complexArray = new Complex[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            complexArray[i] = Complex.of(i);
+        }
+
+        return complexArray;
+    }
+
+    /**
+     * Creates the polar form of a complex number.
+     *
+     * @param rho   absolute value
+     * @param theta angle
+     * @return the complex number in polar form
+     */
+    public static Complex polar(double rho, double theta) {
+        return (new Complex(rho * Math.cos(theta), rho * Math.sin(theta)));
+    }
+
+    /**
+     * Adds a complex number to the present one.
+     *
+     * @param b complex number to add
+     * @return a new complex number containing the result of the addition
+     */
     public Complex add(Complex b) {
-        return new Complex(this.re + b.re, this.im + b.im);
+        return new Complex(real + b.real, imaginary + b.imaginary);
     }
 
-    public Complex sub(Complex b) {
-        return new Complex(this.re - b.re, this.im - b.im);
+    /**
+     * Subtracts a complex number from the present one.
+     *
+     * @param b complex number to subtract
+     * @return a new complex number containing the result of the subtraction
+     */
+    public Complex subtract(Complex b) {
+        return new Complex(real - b.real, imaginary - b.imaginary);
     }
 
-    public Complex mult(Complex b) {
-        return new Complex(this.re * b.re - this.im * b.im,
-            this.re * b.im + this.im * b.re);
+    /**
+     * Multiplies a complex number to the present one.
+     *
+     * @param b complex number to multiply
+     * @return a new complex number containing the result of the multiplication
+     */
+    public Complex multiply(Complex b) {
+        return new Complex(real * b.real - imaginary * b.imaginary, real * b.imaginary + imaginary * b.real);
+    }
+
+    /**
+     * @return the complex conjugate (negative imaginary)
+     */
+    public Complex conjugate() {
+        return new Complex(real, -imaginary);
     }
 
     @Override
     public String toString() {
-        return String.format("(%.6f, %.6f i)", re, im);
+        return String.format("(%.6f, %.6f i)", real, imaginary);
     }
 }
--- src/main/java/de/dhbw/silencio/audio/Microphone.java
+++ src/main/java/de/dhbw/silencio/audio/Microphone.java
@@ -5,6 +5,23 @@ import lombok.*;
 import javax.sound.sampled.*;
 import java.io.Closeable;
 
+/**
+ * Provides access to a microphone.
+ * <p>
+ * This class uses the Java Sound API ({@link TargetDataLine}, {@link AudioFormat}, {@link Mixer}) to read data from a
+ * microphone. When creating a {@link Microphone}, you have to provide a {@link #deviceName}. This is the name of the
+ * microphone in your computer's system settings. How to get this string depends on your operating system. On macOS, for
+ * example, you can find the device name in the <i>Audio MIDI Setup</i> application. You also have to provide an
+ * {@link AudioFormat} that contains configuration on how the data should be read and structured.
+ * <p>
+ * To make sure the {@link TargetDataLine} is closed, when not needed anymore, this class implements {@link Closeable}.
+ * Make sure using try-with resources to probably close all resources. In case you want to work with multiple
+ * microphones, this class implements {@link Runnable}, reading the data in the background.
+ *
+ * @author Yannick Kirschen
+ * @author Moritz Thoma
+ * @since 1.0.0
+ */
 @RequiredArgsConstructor
 public class Microphone implements Closeable, Runnable {
     @Getter
@@ -15,6 +32,12 @@ public class Microphone implements Closeable, Runnable {
 
     private TargetDataLine line;
 
+    /**
+     * Prints all available audio devices.
+     * <p>
+     * This is a convenience method, that uses Java's {@link Mixer.Info} to get all devices connected to the computer.
+     * By using this method, you can get the {@link #deviceName} needed for instantiating a {@link Microphone}.
+     */
     public static void printAvailableDevices() {
         Line.Info line = new Line.Info(TargetDataLine.class);
         for (Mixer.Info info : AudioSystem.getMixerInfo()) {
@@ -26,7 +49,54 @@ public class Microphone implements Closeable, Runnable {
         }
     }
 
-    public static Mixer getMixer(String deviceName) throws IllegalArgumentException {
+    /**
+     * Prepares the audio device to be read.
+     * <p>
+     * To be precise, a {@link TargetDataLine} is created based on the {@link AudioFormat} and {@link #deviceName}. The
+     * line is opened, which does not mean, that data is processed.
+     */
+    public void prepare() {
+        Line.Info info = new DataLine.Info(TargetDataLine.class, format);
+        Mixer mixer = getMixer(deviceName);
+
+        try {
+            line = (TargetDataLine) mixer.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException e) {
+            throw new IllegalArgumentException("Line is not available (%s)".formatted(e.getMessage()));
+        }
+    }
+
+    /**
+     * Copies data from the internal buffer into a byte array.
+     *
+     * @param bufferSize length of the byte array to copy the data into
+     * @return a byte array containing data from the buffer
+     */
+    public byte[] getNextChunk(int bufferSize) {
+        byte[] data = new byte[bufferSize];
+        line.read(data, 0, bufferSize);
+        return data;
+    }
+
+    /**
+     * Stops listening on the {@link TargetDataLine}.
+     */
+    public void stopListen() {
+        line.stop();
+    }
+
+    @Override
+    public void close() {
+        line.close();
+    }
+
+    @Override
+    public void run() {
+        line.start();
+    }
+
+    private Mixer getMixer(String deviceName) throws IllegalArgumentException {
         Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
         Line.Info lineInfo = new Line.Info(TargetDataLine.class);
 
@@ -40,38 +110,6 @@ public class Microphone implements Closeable, Runnable {
         }
 
         throw new IllegalArgumentException("Device %s not found.".formatted(deviceName));
-    }
-
-    public void prepare() {
-        Line.Info info = new DataLine.Info(TargetDataLine.class, format);
-        Mixer mixer = getMixer(deviceName);
-
-        try {
-            line = (TargetDataLine) mixer.getLine(info);
-            line.open(format);
-        } catch (LineUnavailableException e) {
-            throw new IllegalArgumentException("Line is not available (%s)".formatted(e.getMessage()));
-        }
-    }
-
-    public byte[] getNextChunk(int bufferSize) {
-        byte[] data = new byte[bufferSize];
-        line.read(data, 0, data.length);
-        return data;
-    }
-
-    public void stopListen() {
-        line.stop();
-    }
-
-    @Override
-    public void close() {
-        line.close();
-    }
-
-    @Override
-    public void run() {
-        line.start();
     }
 }
 
