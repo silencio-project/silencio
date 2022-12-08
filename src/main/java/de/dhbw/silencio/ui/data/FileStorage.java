b6f56ef9a0d8127cafd25051e5c0c0673e364be1
Paul Antoni <paul.antoni@siemens-healthineers.com>
2022-12-08 14:04:50 +0100

    feat: add import functionality to FileStorage class

--- src/main/java/de/dhbw/silencio/ui/data/FileStorage.java
+++ src/main/java/de/dhbw/silencio/ui/data/FileStorage.java
@@ -1,9 +1,14 @@
 package de.dhbw.silencio.ui.data;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
@@ -23,13 +28,42 @@ public class FileStorage {
         mappingStrategyRoom = new ColumnPositionMappingStrategy();
         mappingStrategyRoom.setType(Room.class);
     }
-//    public ArrayList<Protocol> getAllProtocols{
-//
-//    }
-//
-//    public ArrayList<Room> getAllRooms{
-//
-//    }
+
+    public ArrayList<Protocol> getAllProtocols(){
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader(filenameProtocol));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] columns = new String[] {"roomUid", "data", "time"};
+        mappingStrategyProtocol.setColumnMapping(columns);
+
+        CsvToBean csvToBean = new CsvToBean();
+
+        ArrayList<Protocol> protocolList = (ArrayList<Protocol>) csvToBean.parse(mappingStrategyProtocol, csvReader);
+        return protocolList;
+    }
+
+    public ArrayList<Room> getAllRooms(){
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader(filenameRoom));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] columns = new String[] {"uid", "description", "length", "width", "deskDistanceToBoard", "deskDistanceToWallLeft", "distanceFirstRowToDesk", "tableLength", "tableWidth", "rows", "tablesPerRow", "betweenRows", "distanceToWallLeft"};
+        mappingStrategyRoom.setColumnMapping(columns);
+
+        CsvToBean csvToBean = new CsvToBean();
+
+        ArrayList<Room> roomList = (ArrayList<Room>) csvToBean.parse(mappingStrategyRoom, csvReader);
+        return roomList;
+    }
+
+
     //TODO exceptionhandling
     public void storeProtocol(Protocol protocol){
         try {
